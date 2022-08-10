afee0625a200363977577dd68df7a269e0bdb0ca
noppefoxwolf <noppelabs@gmail.com>
2022-08-11 00:24:39 +0900

    Update Pixel.swift

--- Sources/BitmapContext/Primitive/Pixel.swift
+++ Sources/BitmapContext/Primitive/Pixel.swift
@@ -6,9 +6,25 @@ public protocol Pixel: Equatable {
 public struct RGBAPixel: Pixel {
     public typealias ComponentType = UInt8
     public var red, green, blue, alpha: ComponentType
+    
+    public init(
+        red: RGBAPixel.ComponentType,
+        green: RGBAPixel.ComponentType,
+        blue: RGBAPixel.ComponentType,
+        alpha: RGBAPixel.ComponentType
+    ) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+    }
 }
 
 public struct MonochromePixel: Pixel {
     public typealias ComponentType = UInt8
     public var gray: ComponentType
+    
+    public init(gray: MonochromePixel.ComponentType) {
+        self.gray = gray
+    }
 }

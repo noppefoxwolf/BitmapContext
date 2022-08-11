c7b7dcbdc54f0ae16d0890199cd9d6185289297b
noppefoxwolf <noppelabs@gmail.com>
2022-08-12 04:45:31 +0900

    Update BitmapImage.swift

--- Sources/BitmapContext/BitmapImage.swift
+++ Sources/BitmapContext/BitmapImage.swift
@@ -1,10 +1,15 @@
 import CoreGraphics
 
+@dynamicMemberLookup
 public struct BitmapImage {
     let image: CGImage
     
     @_spi(BitmapExtension)
     public init(image: CGImage) {
         self.image = image
+    }
+    
+    public subscript<S>(dynamicMember keyPath: KeyPath<CGImage, S>) -> S {
+        image[keyPath: keyPath]
     }
 }
